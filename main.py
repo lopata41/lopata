ac448b4db9bdca91f28a8cb08b8a8b3ba6901d31
lopata41 <103258311+lopata41@users.noreply.github.com>
2022-06-07 22:19:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+import requests
 
-sides = sorted(sides, reverse=True)
+city = "Moscow,RU"
+appid = "777ec93d91d18253ffd609643614ba7f"
 
-smax = 0
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
 
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-print("Максимальная площадь треугольника", smax)
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+print("Скорость ветра", data ['wind'] ['speed'])
+print("Видимость", data ["visibility"])
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз погоды на неделю:")
+for i in data['list']:
+ print("Дата <", i['dt_txt'],
+"> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),
+"> \r\nПогодные условия <", i['weather'][0]['description'],
+"> \r\nСкорость Ветра <", i['wind'] ['speed'], ">",
+"> \r\nВидимость <", i['visibility'], ">")
+print("____________________________")
+
+
+
